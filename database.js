998906d7ace8ba9f21a5124cc77c9e7a919a2458
kchange7 <Kchange7@live.unc.edu>
2022-03-23 15:33:23 -0400

    changed data types

--- database.js
+++ database.js
@@ -16,9 +16,7 @@ let row = stmt.get();
 if (row === undefined) {
     //console.log('Your database appears to be empty. I will initialize it now.')
     // Set a const that will contina your SQL commands to initialize the database
-    const sqlInit = `
-        CREATE TABLE accesslog ( remoteaddr TEXT, remoteuser BLOB, time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure TEXT, status INTEGER, referer TEXT, useragent TEXT )
-    `;
+    const sqlInit = `CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser BLOB, time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure INTEGER, status INTEGER, referer TEXT, useragent TEXT )`;
 
     // Execute SQL commands above
     db.exec(sqlInit);
