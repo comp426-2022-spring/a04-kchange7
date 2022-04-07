1521a042e7455d4c6d1720af09a4f6d8dbff8d2e
kchange7 <69439037+kchange7@users.noreply.github.com>
2022-04-07 17:17:51 -0400

    Changed debug check

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (args['help']) {
 
 const HTTP_PORT = args.port ? args.port : 5555;
 const logs = args.log ? args.log : "true";
-const debug = args.debug ? args.debug : false;
+const debug = args.debug ? args.debug : "false";
 
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
@@ -98,7 +98,7 @@ app.get('/app/flip/call/:call', (req, res) => {
     res.json(result);
 });
 
-if (debug) {
+if (debug != "false") {
     app.get('/app/log/access', (req, res) => {
         try {
             const stmt = db.prepare('SELECT * FROM accesslog').all()
